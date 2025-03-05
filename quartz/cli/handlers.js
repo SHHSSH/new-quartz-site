aa57a7d6cba5a538cd8d2accb596ba443c92b96b
Thomas Jefferson <r3v@tuta.io>
2025-03-05 23:32:00 +1000

    Quartz sync: Mar 5, 2025, 11:32 PM

--- quartz/cli/handlers.js
+++ quartz/cli/handlers.js
@@ -155,7 +155,7 @@ export async function handleCreate(argv) {
     await fs.promises.writeFile(
       path.join(contentFolder, "index.md"),
       `---
-title: Welcome to Quartz
+title: Welcome to QuartzNANANANANANANANANA
 ---
 
 This is a blank Quartz installation.
